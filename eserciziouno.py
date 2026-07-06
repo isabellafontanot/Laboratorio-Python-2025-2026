5616e4793d33fa276bb09f80360e739f6106c23b
isabellafontanot <fontanotisabella@gmail.com>
2026-07-06 16:16:19 +0000

    salvo punto uno es uno

---
+++ eserciziouno.py
@@ -0,0 +1,15 @@
+#punto 1
+def is_pari(n): 
+    '''controllo se il numero è pari'''
+
+    if n%2 == 0:
+        return True
+    else:
+        return False
+        
+res = is_pari(4)
+print(res)
+res = is_pari(5)
+print(res)
+
+#punto 2 
