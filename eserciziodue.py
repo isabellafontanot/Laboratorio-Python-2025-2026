94007ea3ccbc91e1398119c3abc6fb6b07b958a9
isabellafontanot <fontanotisabella@gmail.com>
2026-07-10 06:16:20 +0000

    salvo es due ma ricontrollo

--- eserciziodue.py
+++ eserciziodue.py
@@ -82,3 +82,72 @@ righe_invertite = righe[::-1]
 
 testo_righe_invertite = "\n".join(righe_invertite)
 print("Testo con righe invertite:\n", testo_righe_invertite)
+
+#punto 8 
+strofe_modificate = []
+for strofa in testo: 
+    nuova_strofa = list(strofa) 
+    if len(nuova_strofa) > 1: 
+        nuova_strofa[1] = nuova_strofa[1][::-1]
+    strofe_modificate.append("\n".join(nuova_strofa))
+
+testo_specchio = "\n\n".join(strofe_modificate)
+print(testo_specchio)
+
+#punto 9 
+def estrai_parole(testo_da_pulire):
+   '''Tolgo punteggiatura e maiuscole'''
+   pulito = re.sub(r"[^a-zA-Z0-9\s]", " ", testo_da_pulire.lower())
+   return pulito.split()
+
+parole_per_strofa = [set(estrai_parole(" ".join(s))) for s in strofa]
+parole_comuni = set.intersection(*parole_per_strofa)
+
+print("Parole comuni a tutte le strofe:", list(parole_comuni))
+
+#punto 10 
+testo_minuscolo = testo.lower()
+testo_senza_punteggiatura = re.sub(r"[^a-zA-Z0-9\s]", " ", testo_minuscolo)
+tutte_le_parole = testo_senza_punteggiatura.split()
+
+print("Numero di parole totali (con duplicati):", len(tutte_le_parole))
+
+insieme_parole_uniche = set(tutte_le_parole)
+lista_parole_uniche = list(insieme_parole_uniche)
+
+print("Numero di parole uniche (senza duplicati):", len(lista_parole_uniche))
+
+lista_ordinata = sorted(lista_parole_uniche, key=lambda word: (len(word), word))
+
+print("Lista finale")
+print(lista_ordinata)
+
+#punto 11
+freq_caratteri = {}
+
+for carattere in testo:
+    freq_caratteri[carattere] = freq_caratteri.get(carattere, 0) + 1
+
+print("Conteggio di tutti i caratteri")
+
+for chiave in sorted(freq_caratteri.keys()):
+    template = "Carattere {} trovato {} volte." 
+    out = template.format(repr(chiave), freq_caratteri[chiave])
+    print(out)
+
+#punto 12 
+
+freq_alfanumerici = {}
+
+testo_minuscolo = testo.lower()
+
+for carattere in testo_minuscolo:
+    if carattere.isalnum():
+        freq_alfanumerici[carattere] = freq_alfanumerici.get(carattere, 0) + 1
+
+print("Solo caratteri alfanumerici")
+
+for chiave in sorted(freq_alfnumerici.keys()):
+    template = "Lettera {} trovata {} volte." 
+    out = template.format(chiave, freq_alfanumerici[chiave])
+    print(out)
